9a13fee9f244fbcaac06b6a1eaaba6c758b1587f
JuanCarlosPoligonoSur <juan.ruz.franco.al@iespoligonosur.org>
2026-09-25 18:47:00 +0200

    Ejercicio 2

--- unidad1/src/main/java/com/ada/Coleccion1/Ejercicio2.java
+++ unidad1/src/main/java/com/ada/Coleccion1/Ejercicio2.java
@@ -6,27 +6,49 @@ package com.ada.Coleccion1;
 import java.io.*;
 
 public class Ejercicio2 {
+
+    // método para listar todo el contenido de una carpeta
+    public static void listar(File carpeta) {
+
+    //Hago un array con los nombre de los elementos en la carpeta.
+    String[] nombres = carpeta.list();
+
+    //Si el array no está vacío, hago un for each con el nombre de cada elemento
+    if (nombres != null) {
+        for (String nombre : nombres) {
+
+            //creo un objeto file llamado elemento con cada nombre encontrado en carpeta
+            File elemento = new File(carpeta, nombre);
+            
+            //Imprimo el nombre de cada elemento.
+            System.out.println(elemento.getName());
+
+            //Una función que he encontrado consigue la ruta total del elemento  .getAbsolutePath()
+
+            //Si el elemento es una carpeta, vuelvo a llamar al método listar
+            if (elemento.isDirectory()) {
+                listar(elemento);
+            }
+        }
+    }
+    }
+
+    
     public static void main(String[] args){
 
     //Creo la ruta a escritorio
     String escritorio = System.getProperty("user.home") + File.separator + "Desktop";
 
-    //Creo el objeto File d en la ruta escritorio
-    File e= new File(escritorio);  
-
-    //Al objeto file le aplico list(),devuelve un array de cadenas de  caracteres con los nombres de los ficheros 
-    // y directorios dentro del fichero asociado al objeto File  
-
-    String[] contenido_E=e.list(); 
+    //Creo el objeto File llamado carpeta con la ruta de escritorio
+    File carpeta= new File(escritorio);  
 
 
-    System.out.println("El contenido de "+ d.getName()+" : ");
-
-    for(String a:contenido_d) { 
-        System.out.println(a);
-    }
+    //llamo al metodo listar
+    listar(carpeta);
 
 
+
+    
 }  
 
    
