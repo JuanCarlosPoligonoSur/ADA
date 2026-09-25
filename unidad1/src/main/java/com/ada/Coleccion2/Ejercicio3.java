fb22ce8723b32f42825f8fa36ad520d26f297d47
JuanCarlosPoligonoSur <juan.ruz.franco.al@iespoligonosur.org>
2026-09-25 21:01:07 +0200

    Cambiando el paquete funciona, preguntar por qué

---
+++ unidad1/src/main/java/com/ada/Coleccion2/Ejercicio3.java
@@ -0,0 +1,35 @@
+//Lee un fichero.
+
+package main.java.com.ada.Coleccion2;
+import java.io.*;
+
+public class Ejercicio3 {
+
+   //Sin lanzar la excepción, no funciona el programa con un FileWriter
+    public static void main(String[] args) throws IOException {
+        String frase = "Hola Mundo";
+
+        String rutaArchivo = System.getProperty("user.home") + File.separator + "Desktop"+File.separator+"HOLA.TXT";
+
+        //El append true permite añadir una línea cada vez que se ejecute el fichero. Si no lo pongo, lo machaca.
+        // YA CUMPLÍA EN EL ejercicio1 lo de volver a poder a escribir.
+        FileWriter fw= new FileWriter(rutaArchivo,true);
+        PrintWriter pw= new PrintWriter(fw);
+
+
+        pw.println(frase);
+
+        //Poner siempre el flush y el close. flush limpia y close lo cierra en memoria. 
+        pw.flush();
+        pw.close();
+        
+        //Me quedo aquí. Repasar bien.
+        BufferedReader fr= new BufferedReader();
+
+        while(fr.readLine()!-1){
+        fr.readLine();
+        }
+
+    }
+    
+}
