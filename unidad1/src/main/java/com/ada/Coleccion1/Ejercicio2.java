9c7eb7b91b40932543b64088631b6698ad97d083
JuanCarlosPoligonoSur <juan.ruz.franco.al@iespoligonosur.org>
2026-09-25 17:59:37 +0200

    Ejercicio 2

--- unidad1/src/main/java/com/ada/Coleccion1/Ejercicio2.java
+++ unidad1/src/main/java/com/ada/Coleccion1/Ejercicio2.java
@@ -6,6 +6,28 @@ package com.ada.Coleccion1;
 import java.io.*;
 
 public class Ejercicio2 {
+    public static void main(String[] args){
 
-    // continuo por aquí.
+    //Creo la ruta a escritorio
+    String escritorio = System.getProperty("user.home") + File.separator + "Desktop";
+
+    //Creo el objeto File d en la ruta escritorio
+    File e= new File(escritorio);  
+
+    //Al objeto file le aplico list(),devuelve un array de cadenas de  caracteres con los nombres de los ficheros 
+    // y directorios dentro del fichero asociado al objeto File  
+
+    String[] contenido_E=e.list(); 
+
+
+    System.out.println("El contenido de "+ d.getName()+" : ");
+
+    for(String a:contenido_d) { 
+        System.out.println(a);
+    }
+
+
+}  
+
+   
 }
