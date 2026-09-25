694d5008d810ecab6170f6c8d24b69819a2aa9b3
Juan Carlos <juan.ruz.franco.al@poligonosur.org>
2026-09-25 13:04:12 +0200

    cominezo ejercicio 2

---
+++ unidad1/src/main/java/com/ada/Coleccion1/Ejercicio2.java
@@ -0,0 +1,11 @@
+//2. Crea un método de Java que dado como entrada el directorio raíz del ejercicio 1, 
+// liste los ficheros y directorios del mismo, mostrando la misma información que se ve
+//  en el ejercicio 1.
+
+package com.ada.Coleccion1;
+import java.io.*;
+
+public class Ejercicio2 {
+
+    // continuo por aquí.
+}
