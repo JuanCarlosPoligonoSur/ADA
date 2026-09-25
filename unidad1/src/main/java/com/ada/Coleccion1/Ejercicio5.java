bbeddf706750c9b17a286bbb3cd39eaa2b4e788f
JuanCarlosPoligonoSur <juan.ruz.franco.al@iespoligonosur.org>
2026-09-25 19:56:35 +0200

    Ejercicio 5

---
+++ unidad1/src/main/java/com/ada/Coleccion1/Ejercicio5.java
@@ -0,0 +1,48 @@
+//5. Escribe un método que borre todos los ficheros que tienen la extensión txt 
+// dentro de un directorio que se pasará como parámetro.
+
+package main.java.com.ada.Coleccion1;
+import java.io.File;
+
+
+public class Ejercicio5 {
+
+
+    public static void BorrarPorExtension(File carpeta, String extension) {
+
+        //Creo array con todos los elementos de la carpeta
+        String[] nombres = carpeta.list();
+
+        //Si hay elementos en el array, creo para cada uno de ellos un Objeto File elemento.
+        if (nombres != null) {
+            for (String nombre : nombres) {
+
+                File elemento = new File(carpeta, nombre);
+
+                String nombreArchivo=elemento.getName();
+
+                //Si es un archivo y el nombre termina en .extension, lo borra. .delete() devuelve true cuando lo elimina
+                if (elemento.isFile() && nombre.endsWith("." + extension)&& elemento.delete()) {
+                    
+                    System.out.println("El archivo: " + nombreArchivo+" se ha borrado correctamente");
+                }
+            }
+        }
+    }
+
+
+public static void main(String[] args){
+
+    //Creo la ruta a la carpeta
+    String carpetaAleatoria = System.getProperty("user.home") + File.separator + "Documentos";
+
+    //Creo el objeto File llamado carpeta con la ruta deseada
+    File carpeta= new File(carpetaAleatoria);  
+
+
+    //llamo al metodo listar
+    BorrarPorExtension(carpeta,"txt");
+
+    }
+}
+
